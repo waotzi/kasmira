612136b7ed009b2b2e2d78095cd1d4625260d186
waotzi <waotzi@proton.me>
2023-03-24 13:02:18 +0700

    update logo

--- src/components/templates/BaseTemplate.ts
+++ src/components/templates/BaseTemplate.ts
@@ -3,15 +3,20 @@ export default (slot: string) => `
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=devic
+        e-width, initial-scale=1.0">
         <title>Kasmira</title>
         <meta name="description" content="manage your kas portfolio">
         <meta name="author" content="waotzi">    
         <link rel="stylesheet" href="/public/style.css">
+        <link rel="icon" type="image/png" href="/public/favicon.png" />
     </head>
     <body>
       <header>
-        <h1><a href="/">Kasmira</a></h1>
+        <a href="/" class="logo">
+          <span class="logo-image"></span>
+          <h1>asmira</h1>
+        </a>
       </header>
       <nav>
           <ul>
