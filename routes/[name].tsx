d0dd1696f2bcc9e17449c0ec80126276ad14551c
waotzi <waotzi@proton.me>
2023-04-25 16:10:30 +0800

    center profile name

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -1,5 +1,11 @@
 import { PageProps } from "$fresh/server.ts";
 
 export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+  return <div>
+    <br/>
+    <h1>
+    {props.params.name}
+    </h1>
+    <br/>
+  </div>;
 }
